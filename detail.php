b0220fdc4bd37fe713f5dc22a12858016a6d21a3
SuryaBlack6 <b3ntol34@yahoo.com>
2012-11-03 15:30:52 +0700

    file masuk

---
+++ detail.php
@@ -0,0 +1,101 @@
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" media="screen,projection,print" href="./css/layout_admin.css" />
+  <link rel="stylesheet" type="text/css" media="screen,projection,print" href="./css/layout_admin_text.css" />
+  <link rel="icon" type="image/x-icon" href="./img/favicon.ico" />
+  <title>Odontogram Application System</title>
+</head>
+
+<!-- Global IE fix to avoid layout crash when single word size wider than column width -->
+<!--[if IE]><style type="text/css"> body {word-wrap: break-word;}</style><![endif]-->
+
+<body>
+  <div class="page-container">      
+    <div class="header">
+      <div class="header-top">
+        <a class="sitelogo" href="#"></a>
+        <div class="sitename">
+          <h1><a href="index.html">Odontogram</a></h1>
+          <h2>Odontogram Application System</h2>
+        </div>
+      </div>
+      <div class="header-bottom">
+	  </div>
+    </div>
+    <div class="main">
+      <div class="main-navigation">
+        <div class="round-border-topright"></div>
+        <h1 class="first">Sidebar</h1>
+        <dl class="nav3-grid">
+          <dt><a href="http://localhost/odontogram">Home</a></dt>
+		  <dt><a href="http://localhost/odontogram/detail.php">Detail Gigi</a></dt>
+		  <dt><a href="http://localhost/odontogram/detail2.php">Detail Gigi 2</a></dt>
+        </dl>                       
+      </div>
+      <div class="main-content">
+        <h1 class="pagetitle">Odontogram</h1>       
+        <div class="column1-unit">        
+          <h1>Odontogram Application System</h1>                            
+          <h3>Portings already available or in development</h3>
+          </div>  
+		<!--Map Begin-->
+		 <div>  
+<MAP NAME=mymap>
+<area shape="rect" alt="Detail Gigi 1" title="Detail Gigi 1" coords="45,15,204,44" href="" target="" />
+<area shape="rect" alt="Detail Gigi 2" title="Detail Gigi 2" coords="46,47,206,201" href="" target="" />
+<area shape="rect" alt="Detail Gigi 3" title="Detail Gigi 3" coords="206,48,235,202" href="" target="" />
+<area shape="rect" alt="Detail Gigi 4" title="Detail Gigi 4" coords="45,203,204,231" href="" target="" />
+<area shape="rect" alt="Detail Gigi 5" title="Detail Gigi 5" coords="13,46,44,201" href="" target="" />
+</MAP>
+<IMG SRC="images/gigi-1.jpg" ALT="Site map" USEMAP="#mymap" WIDTH=250 HEIGHT=250>
+<div class="clear"></div>
+<input id="submit" class="button" type="submit" tabindex="6" value="Publish" name="submit">
+<input class="button" type="reset" tabindex="6" value="reset" name="reset">
+</div>
+<div class="clear"></div>
+<h2>Keterangan Odontogram</h2>
+<table style="width:650px;height:70px;">
+<tr>
+<th class="top" scope="col">Belum Erupsi</th>
+				<th class="top" scope="col">Sudah Dicabut</th>
+				<th class="top" scope="col">Goyah</th>
+				<th class="top" scope="col">Tinggal Akar</th>
+				<th class="top" scope="col">Karies</th>
+				<th class="top" scope="col">Tumpatan</th>
+				</tr>
+<tr>
+<td>
+<img src="images/bt1.png">
+</td>
+<td>
+<img src="images/bt2.png">
+</td>
+<td>
+<img src="images/bt3.png">
+</td>
+<td>
+<img src="images/bt4.png">
+</td>
+<td>
+<img src="images/bt5.png">
+</td>
+<td>
+<img src="images/bt6.png">
+</td>
+</tr>
+</table>
+		<!--Map End-->  
+        <hr class="clear-contentunit" />
+      </div> 
+    </div>
+    <div class="footer">
+      <p>Copyright &copy; 2012 Odontogram Application System Information | All Rights Reserved</p>
+      <p class="credits">Design by &copy;<a href="http://www.jogjadev.com/">JogjaDev</a></p>
+    </div>      
+  </div> 
+  
+</body>
+</html>
+
+
+
---
+++ index.php
@@ -0,0 +1,107 @@
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" media="screen,projection,print" href="./css/layout_admin.css" />
+  <link rel="stylesheet" type="text/css" media="screen,projection,print" href="./css/layout_admin_text.css" />
+  <link rel="icon" type="image/x-icon" href="./img/favicon.ico" />
+  <title>Odontogram Application System</title>
+</head>
+
+<!-- Global IE fix to avoid layout crash when single word size wider than column width -->
+<!--[if IE]><style type="text/css"> body {word-wrap: break-word;}</style><![endif]-->
+
+<body>
+  <div class="page-container">      
+    <div class="header">
+      <div class="header-top">
+        <a class="sitelogo" href="#"></a>
+        <div class="sitename">
+          <h1><a href="index.html">Odontogram</a></h1>
+          <h2>Odontogram Application System</h2>
+        </div>
+      </div>
+      <div class="header-bottom">
+	  </div>
+    </div>
+    <div class="main">
+      <div class="main-navigation">
+        <div class="round-border-topright"></div>
+        <h1 class="first">Sidebar</h1>
+        <dl class="nav3-grid">
+          <dt><a href="http://localhost/odontogram">Home</a></dt>
+		  <dt><a href="http://localhost/odontogram/detail.php">Detail Gigi</a></dt>
+		  <dt><a href="http://localhost/odontogram/detail2.php">Detail Gigi 2</a></dt>
+        </dl>                       
+      </div>
+      <div class="main-content">
+        <h1 class="pagetitle">Odontogram</h1>       
+        <div class="column1-unit">        
+          <h1>Odontogram Application System</h1>                            
+          <h3>Portings already available or in development</h3>
+          </div>  
+		<!--Map Begin-->
+<MAP NAME=mymap>
+  <area shape="rect" alt="Gigi 1" title="Gigi 1" coords="19,24,51,69" href="http://localhost/odontogram/detail.php" target="" />
+    <area shape="rect" alt="Gigi 2" title="Gigi 2" coords="61,24,95,69" href="#" target="" />
+    <area shape="rect" alt="Gigi 3" title="Gigi 3" coords="104,24,138,69" href="#" target="" />
+    <area shape="rect" alt="Gigi 4" title="Gigi 4" coords="147,24,182,69" href="#" target="" />
+    <area shape="rect" alt="Gigi 5" title="Gigi 5" coords="191,24,224,69" href="http://localhost/odontogram/detail2.php" target="" />
+    <area shape="rect" alt="Gigi 6" title="Gigi 6" coords="234,24,268,68" href="#" target="" />
+    <area shape="rect" alt="Gigi 7" title="Gigi 7" coords="277,24,310,68" href="#" target="" />
+    <area shape="rect" alt="Gigi 8" title="Gigi 8" coords="339,25,372,69" href="#" target="" />
+    <area shape="rect" alt="Gigi 9" title="Gigi 9" coords="381,25,414,68" href="#" target="" />
+    <area shape="rect" alt="Gigi 10" title="Gigi 10" coords="425,25,459,68" href="#" target="" />
+    <area shape="rect" alt="Gigi 11" title="Gigi 11" coords="468,25,502,69" href="#" target="" />
+    <area shape="rect" alt="Gigi 12" title="Gigi 12" coords="512,25,545,68" href="#" target="" />
+    <area shape="rect" alt="Gigi 13" title="Gigi 13" coords="555,24,588,68" href="#" target="" />
+    <area shape="rect" alt="Gigi 14" title="Gigi 14" coords="599,23,631,69" href="#" target="" />
+    <area shape="rect" alt="Gigi 15" title="Gigi 15" coords="104,87,138,131" href="#" target="" />
+    <area shape="rect" alt="Gigi 16" title="Gigi 16" coords="149,87,182,131" href="#" target="" />
+    <area shape="rect" alt="Gigi 17" title="Gigi 17" coords="192,88,224,131" href="#" target="" />
+    <area shape="rect" alt="Gigi 18" title="Gigi 18" coords="235,88,268,131" href="#" target="" />
+    <area shape="rect" alt="Gigi 19" title="Gigi 19" coords="278,88,310,131" href="#" target="" />
+    <area shape="rect" alt="Gigi 20" title="Gigi 20" coords="340,88,373,131" href="#" target="" />
+    <area shape="rect" alt="Gigi 21" title="Gigi 21" coords="383,88,416,131" href="#" target="" />
+    <area shape="rect" alt="Gigi 22" title="Gigi 22" coords="426,88,458,131" href="#" target="" />
+    <area shape="rect" alt="Gigi 23" title="Gigi 23" coords="469,87,501,131" href="#" target="" />
+    <area shape="rect" alt="Gigi 24" title="Gigi 24" coords="512,87,544,130" href="#" target="" />
+    <area shape="rect" alt="Gigi 25" title="Gigi 25" coords="104,167,138,213" href="#" target="" />
+    <area shape="rect" alt="Gigi 26" title="Gigi 26" coords="148,168,181,213" href="#" target="" />
+    <area shape="rect" alt="Gigi 27" title="Gigi 27" coords="192,167,226,212" href="#" target="" />
+    <area shape="rect" alt="Gigi 28" title="Gigi 28" coords="235,168,268,212" href="#" target="" />
+    <area shape="rect" alt="Gigi 29" title="Gigi 29" coords="278,169,311,212" href="#" target="" />
+    <area shape="rect" alt="Gigi 30" title="Gigi 30" coords="340,168,373,211" href="#" target="" />
+    <area shape="rect" alt="Gigi 31" title="Gigi 31" coords="382,168,415,211" href="#" target="" />
+    <area shape="rect" alt="Gigi 32" title="Gigi 32" coords="426,168,458,212" href="#" target="" />
+    <area shape="rect" alt="Gigi 33" title="Gigi 33" coords="469,168,501,212" href="#" target="" />
+    <area shape="rect" alt="Gigi 34" title="Gigi 34" coords="512,167,545,212" href="#" target="" />
+    <area shape="rect" alt="Gigi 35" title="Gigi 35" coords="18,231,51,274" href="#" target="" />
+    <area shape="rect" alt="Gigi 36" title="Gigi 36" coords="60,231,95,274" href="#" target="" />
+    <area shape="rect" alt="Gigi 37" title="Gigi 37" coords="104,231,137,274" href="#" target="" />
+    <area shape="rect" alt="Gigi 38" title="Gigi 38" coords="147,230,181,275" href="#" target="" />
+    <area shape="rect" alt="Gigi 39" title="Gigi 39" coords="190,230,224,275" href="#" target="" />
+    <area shape="rect" alt="Gigi 40" title="Gigi 40" coords="234,230,268,274" href="#" target="" />
+    <area shape="rect" alt="Gigi 41" title="Gigi 41" coords="277,230,310,274" href="#" target="" />
+    <area shape="rect" alt="Gigi 42" title="Gigi 42" coords="338,230,371,274" href="#" target="" />
+    <area shape="rect" alt="Gigi 43" title="Gigi 43" coords="381,231,415,275" href="#" target="" />
+    <area shape="rect" alt="Gigi 44" title="Gigi 44" coords="425,231,459,275" href="#" target="" />
+    <area shape="rect" alt="Gigi 45" title="igi 45" coords="469,231,501,274" href="#" target="" />
+    <area shape="rect" alt="Gigi 46" title="Gigi 46" coords="512,231,544,275" href="#" target="" />
+    <area shape="rect" alt="Gigi 47" title="Gigi 47" coords="555,231,587,275" href="#" target="" />
+    <area shape="rect" alt="Gigi 48" title="Gigi 48" coords="599,231,631,273" href="#" target="" />
+</MAP>
+<IMG SRC="images/odontogram.jpg" ALT="Site map" USEMAP="#mymap" WIDTH=650 HEIGHT=300>
+		<!--Map End-->  
+        <hr class="clear-contentunit" />
+      </div> 
+    </div>
+    <div class="footer">
+      <p>Copyright &copy; 2012 Odontogram Application System Information | All Rights Reserved</p>
+      <p class="credits">Design by &copy;<a href="http://www.jogjadev.com/">JogjaDev</a></p>
+    </div>      
+  </div> 
+  
+</body>
+</html>
+
+
+
